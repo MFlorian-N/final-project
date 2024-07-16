9b822be793104ff97a018a0830fd08ae24ca9b60
scarlat-trainer <78.cristi@gmail.com>
2024-07-16 19:56:52 +0300

    fix bootstrap css import

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,10 +1,12 @@
-
+import Button from "react-bootstrap/Button"
 
 
 const Home = () => {
     return(
         <div>
-            
+            <Button>Click me</Button>
         </div>
     )
 }
+
+export default Home;
