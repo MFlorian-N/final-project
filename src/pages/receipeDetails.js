224d20804ba4d275bbd6d723778fd9566a0e2707
MFlorian-N <marinescuflorian1998@gmail.com>
2024-07-24 19:05:59 +0300

    card implementation

--- src/pages/receipeDetails.js
+++ src/pages/receipeDetails.js
@@ -1,7 +1,8 @@
 import { useParams, Link } from "react-router-dom";
-import { Navbar } from "react-bootstrap";
+import { Navbar, Card} from "react-bootstrap";
 import { useEffect, useState } from "react";
-import { getReceipeById } from "../services/spoonaciularApi";
+import { getReceipeById } from "../services/spoonaciularApi"
+import './style.css'
 
 const ReceipeDetails = () => {
     const [data, setData] = useState();
@@ -22,17 +23,22 @@ const ReceipeDetails = () => {
     }, [id])
 
     return (
-        <div>
+        <div className="home-background">
             <Navbar className="bg-light justify-content-between">
                 <Navbar.Brand href='#' className='brand-font'>RecipeRadar</Navbar.Brand>
-                <Link to="/" className="nav-link">Back to Receipes List</Link>
+                <button className="btn-home"><Link to="/" className="nav-link">Back to Receipes List</Link></button>
             </Navbar>
             <main style={{maxWidth: "60rem", margin: "auto"}}>
                 <h1 className="text-center">{data?.title}</h1>
-                <p dangerouslySetInnerHTML={{__html: data?.summary}} className="m-3"></p>
+                    <Card className="recipe-card m-3">
+                        <Card.Img variant="top" src={data.image} alt={data.title} />
+                        <Card.Body>
+                            <Card.Text dangerouslySetInnerHTML={{__html: data.summary}} />
+                        </Card.Body>
+                    </Card>
             </main>
         </div>
     )
 }
 
-export default ReceipeDetails
+export default ReceipeDetails;
